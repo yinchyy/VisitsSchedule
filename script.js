039b21e32cb2feea14a48fc7a6c5f526cb0e41d3
Szymon Studniarz <szymek1019@gmail.com>
2021-11-15 15:30:06 +0100

    add methods to generate calendar buttons
    feat: renderElem handles generating element and sets attributes and inner
    text, it makes it reusable, if no problems occur in the future it's
    going to stay as is.
    
    feat: generateCalendar now generates set of buttons just to begin with
    styling

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 class VisitsSchedule {
     constructor() {
+        this.currentDate = new Date();
+
 
     }
+    generateCalendar(date) {
+        date.GetDay;
+        for (let i = 0; i < 30; ++i) {
+            this.renderElem("button", `${date.getFullYear()}-${date.getMonth()}-${i + 1}`, "dayButton", "dateContainer", i + 1);
+        }
+    }
+    renderElem(elementTag, id, className, elementParentID, elementContent) {
+        if (!document.getElementById(id)) {
+            const element = document.createElement(elementTag);
+            element.setAttribute('id', id);
+            element.setAttribute('class', className);
+            document.getElementById(elementParentID).appendChild(element);
+        }
+        document.getElementById(id).innerText = elementContent;
+    }
 }
+v1 = new VisitsSchedule();
+console.log(v1.generateCalendar(v1.currentDate));
