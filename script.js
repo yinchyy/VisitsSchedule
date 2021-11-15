3d9ce53e87eb4426ffbd2e3b78234d780b81d592
Szymon Studniarz <szymek1019@gmail.com>
2021-11-15 18:37:29 +0100

    generate calendar, display current month and days
    feat: now calendar generates itself with days relating to weekdays
    properly, previous and next month days may also be generated to fill
    possible gaps if there are any. Month name is also relevant to data
    presented below it.

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
 class VisitsSchedule {
     constructor() {
-        this.currentDate = new Date();
+        this.currentMonthDate = new Date();
+        this.today = new Date();
 
-
+        this.currentMonthDate.setDate(1);
+    }
+    initiateMainContainer() {
+        if (!document.getElementById("mainContainer")) {
+            let tmp = document.createElement("div");
+            tmp.setAttribute("id", "mainContainer");
+            document.body.appendChild(tmp);
+        }
     }
     generateCalendar(date) {
-        date.GetDay;
-        for (let i = 0; i < 30; ++i) {
-            this.renderElem("button", `${date.getFullYear()}-${date.getMonth()}-${i + 1}`, "dayButton", "dateContainer", i + 1);
+        this.initiateMainContainer();
+        this.renderElem("div", "calendarBox", null, "mainContainer", null);
+        this.renderElem("div", "monthContainer", null, "calendarBox", null);
+        this.renderElem("button", "previousMonth", null, "monthContainer", "<");
+        this.renderElem("h2", "currentMonth", null, "monthContainer", date.toLocaleDateString("en-GB", { month: 'long' }));
+        this.renderElem("button", "nextMonth", null, "monthContainer", ">");
+        this.renderElem("div", "dateContainer", null, "calendarBox", null);
+        for (let i = 0; i <= 6; ++i) {
+            this.renderElem("div", `d${i}`, `d`, "dateContainer", null);
+            this.renderElem("p", `dayLabel${i}`, `dayLabel`, `d${i}`, new Date(date.getFullYear(), date.getMonth(), i).toLocaleDateString('en-GB', { weekday: 'short' }));
+        }
+        date.setDate(-date.getDay() + 1)
+        for (let i = 0; i <= 34; ++i) {
+            this.renderElem("button", `${date.getFullYear()} - ${date.getMonth()} - ${date.getDate()}`, "dateButton", `d${date.getDay()}`, date.getDate());
+            date.setDate(date.getDate() + 1);
         }
     }
     renderElem(elementTag, id, className, elementParentID, elementContent) {
         if (!document.getElementById(id)) {
             const element = document.createElement(elementTag);
-            element.setAttribute('id', id);
-            element.setAttribute('class', className);
+            if (id) element.setAttribute('id', id);
+            if (className) element.setAttribute('class', className);
             document.getElementById(elementParentID).appendChild(element);
         }
-        document.getElementById(id).innerText = elementContent;
+        if (id && elementContent) document.getElementById(id).innerText = elementContent;
     }
 }
 v1 = new VisitsSchedule();
-console.log(v1.generateCalendar(v1.currentDate));
+console.log(v1.generateCalendar(v1.currentMonthDate));
