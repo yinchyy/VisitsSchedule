29ff874909f2c3eee261b533d9bd822d9b6e3056
Szymon Studniarz <szymek1019@gmail.com>
2021-11-22 14:00:33 +0100

    feat: switch between months, fix: days display in wrong weekdays
    feat: New method switchMonth lets user see calendar for previous
    and next months and switch between them
    
    fix: before this commit days weren't aligned with it's weekdays because
    weekdays numeration is 0-6, where 0 is sunday, and in calendar first day
    is monday, so now it's calculated differently (using modulo operator),
    and works just fine.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ class VisitsSchedule {
     constructor() {
         this.currentMonthDate = new Date();
         this.today = new Date();
-
+        this.selectedLanguage = 'en-US';
         this.currentMonthDate.setDate(1);
     }
     initiateMainContainer() {
@@ -12,23 +12,51 @@ class VisitsSchedule {
             document.body.appendChild(tmp);
         }
     }
+    switchMonth(direction) {
+        try {
+            if (direction === -1 || direction === 1) {
+                console.log(direction);
+                console.log(this.currentMonthDate);
+                this.currentMonthDate.setMonth(this.currentMonthDate.getMonth() + direction);
+                document.getElementById("monthContainer").remove();
+                document.getElementById("dateContainer").remove();
+                this.generateCalendar(this.currentMonthDate);
+            }
+            else throw Error;
+
+        }
+        catch (Error) {
+            console.log("Something is wrong, try again later or check your settings");
+        }
+    }
     generateCalendar(date) {
+        const selectedMonth = date.getMonth();
         this.initiateMainContainer();
         this.renderElem("div", "calendarBox", null, "mainContainer", null);
         this.renderElem("div", "monthContainer", null, "calendarBox", null);
         this.renderElem("button", "previousMonth", null, "monthContainer", "<");
-        this.renderElem("h2", "currentMonth", null, "monthContainer", date.toLocaleDateString("en-GB", { month: 'long' }));
+        document.getElementById("previousMonth").setAttribute("onclick", "v1.switchMonth(-1)");
+        this.renderElem("h2", "currentMonth", null, "monthContainer", `${date.toLocaleDateString(this.selectedLanguage, { month: 'long' })} ${date.getFullYear()}`);
         this.renderElem("button", "nextMonth", null, "monthContainer", ">");
+        document.getElementById("nextMonth").setAttribute("onclick", "v1.switchMonth(1)");
         this.renderElem("div", "dateContainer", null, "calendarBox", null);
         for (let i = 0; i <= 6; ++i) {
             this.renderElem("div", `d${i}`, `d`, "dateContainer", null);
-            this.renderElem("p", `dayLabel${i}`, `dayLabel`, `d${i}`, new Date(date.getFullYear(), date.getMonth(), i).toLocaleDateString('en-GB', { weekday: 'short' }));
+            this.renderElem("p", `dayLabel${i}`, `dayLabel`, `d${i}`, new Date(2020, 11, i).toLocaleDateString(this.selectedLanguage, { weekday: 'short' }));
         }
-        date.setDate(-date.getDay() + 1)
+        date.setDate(-date.getDay() + 2);
         for (let i = 0; i <= 34; ++i) {
-            this.renderElem("button", `${date.getFullYear()} - ${date.getMonth()} - ${date.getDate()}`, "dateButton", `d${date.getDay()}`, date.getDate());
+            this.renderElem("button", `${date.getFullYear()} - ${date.getMonth() + 1} - ${date.getDate()}`, "dateButton", `d${(date.getDay() + 6) % 7}`, date.getDate());
+            if (date.getMonth() != selectedMonth) {
+                document.getElementById(`${date.getFullYear()} - ${date.getMonth() + 1} - ${date.getDate()}`).setAttribute("class", "prevOrNextMonthButton");
+            }
             date.setDate(date.getDate() + 1);
         }
+        if (date.getDate() < 25) {
+            date.setMonth(date.getMonth() - 1);
+        }
+        date.setDate(1);
+        console.log(date);
     }
     renderElem(elementTag, id, className, elementParentID, elementContent) {
         if (!document.getElementById(id)) {
